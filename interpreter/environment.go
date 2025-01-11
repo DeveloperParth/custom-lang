676057690cb30592600fcdc1318386c5bdb4d21c
Parth <parmarparth996@gmail.com>
2025-01-11 15:29:42 +0530

    Refactor Environment to support multiple variable types with Literal struct

--- interpreter/environment.go
+++ interpreter/environment.go
@@ -1,28 +1,77 @@
 package interpreter
 
-type Environment struct {
-	intVars map[string]int64
-	parent  *Environment
+type Datatype string
+
+const (
+	INT  Datatype = "int"
+	BOOL Datatype = "bool"
+	NULL Datatype = "null"
+)
+
+type Literal struct {
+	datatype Datatype
+	value    any
 }
 
-func (e *Environment) getInt(name string) int64 {
-	value, ok := e.intVars[name]
+func NewLiteral(datatype Datatype, value any) Literal {
+	return Literal{
+		datatype: datatype,
+		value:    value,
+	}
+}
+
+type Variable struct {
+	Name string
+	Literal
+}
+
+func (v *Variable) get() any {
+	switch v.Literal.datatype {
+	case INT:
+		return v.Literal.value.(int64)
+	case BOOL:
+		return v.Literal.value.(bool)
+	case NULL:
+		return nil
+	}
+	return nil
+}
+
+type Environment struct {
+	parent *Environment
+	vars   map[string]Variable
+}
+
+func (e *Environment) getOrPanic(name string) Variable {
+	variable, ok := e.get(name)
+	if !ok {
+		panic("Unknown variable")
+	}
+	return variable
+}
+
+func (e *Environment) get(name string) (Variable, bool) {
+	value, ok := e.vars[name]
 	if !ok {
 		if e.parent != nil {
-			return e.parent.getInt(name)
+			return e.parent.get(name)
 		} else {
-			panic("Unknown variable")
+			return Variable{}, false
 		}
 	}
-	return value
+	return value, true
 }
-func (e *Environment) setInt(name string, value int64) {
-	e.intVars[name] = value
+
+func (e *Environment) set(name string, value Literal) {
+	e.vars[name] = Variable{
+		Name:    name,
+		Literal: value,
+	}
 }
 
 func NewEnvironment(parent *Environment) *Environment {
 	return &Environment{
-		intVars: make(map[string]int64),
-		parent:  parent,
+		vars:   make(map[string]Variable),
+		parent: parent,
 	}
 }
